c88e717a54924aef86e5c665d17a1ea8935bcc3d
Okpara Obinna <obainoforgod@gmail.com>
2018-12-29 09:43:22 +0100

    calculation

--- BHEATCK.Forms/Form1.cs
+++ BHEATCK.Forms/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BHEATCK.Forms.Utilities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+using System.Diagnostics;
 
 namespace BHEATCK.Forms
 {
@@ -15,6 +18,39 @@ namespace BHEATCK.Forms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+
+            List<BetForcast> betForcasts = new List<BetForcast>();
+            using (TextFieldParser csvParser = new TextFieldParser("test.csv"))
+            {
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                // Skip the row with the column names
+                string[] fields = csvParser.ReadFields();
+
+                Debug.Assert(fields != null, nameof(fields) + " != null");
+
+                while (!csvParser.EndOfData)
+                {
+                    // Read current line fields, pointer moves to the next line.
+                    fields = csvParser.ReadFields();
+                    if (!string.IsNullOrWhiteSpace(fields[1]))
+                    {
+                        betForcasts.Add(BetForcast.GetForcast(fields[0], Convert.ToDouble(fields[4]), Convert.ToDouble(fields[1])));
+                        betForcasts.Add(BetForcast.GetForcast(fields[0], Convert.ToDouble(fields[2]), Convert.ToDouble(fields[3])));
+                    }
+                }
+
+                dataGridView1.DataSource = betForcasts;
+
+                Cursor = Cursors.Arrow;
+            }
         }
     }
 }
---
+++ BHEATCK.Forms/Utilities/BetForcast.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHEATCK.Forms.Utilities
+{
+    class BetForcast
+    {
+        public string Match { get; set; }
+        public double OverOdd { get; set; }
+        public double UnderOdd { get; set; }
+        public double PercentageStake1 { get; set; }
+        public double PercentageStake2 { get; set; }
+        public double Gain { get; set; }
+
+        public static BetForcast GetForcast(string m, double overOdd, double underOdd)
+        {
+            var ps1 = 0.0;
+            var ps2 = 0.0;
+            var g = 0.0;
+
+            Calculator.getStake(overOdd, underOdd, out ps1, out ps2, out g);
+
+            var rtn = new BetForcast()
+            {
+                Gain = g,
+                Match=m,
+                OverOdd= overOdd,
+                UnderOdd = underOdd,
+                PercentageStake1 = ps1,
+                PercentageStake2=ps2,
+            };
+
+            return rtn;
+        }
+        
+
+    }
+}
